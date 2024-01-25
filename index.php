d96844cc0ac777624574783a4b369921e7b63717
nicomanunta <nic.manun@gmail.com>
2024-01-25 16:26:26 +0100

    Secondo Bonus completato

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
     include __DIR__."/partials/array_hotel.php";
 
+    $filter_hotel = $hotels;
+    if(isset($_GET["parking"])){
+        $filter_hotel = [];
+        $parking = $_GET["parking"];
+        foreach ($hotels as $hotel){
+            var_dump($parking);
+            var_dump($hotel["parking"]);
+            if ($hotel["parking"] == $parking){
+                $filter_hotel [] = $hotel;
+                // var_dump($filter_hotel);
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -14,36 +27,54 @@
 </head>
 <body>
     <?php include __DIR__."/partials/templates/header.php" ?>
-    <div class="container my-5">
-        <div class="row">
-            <div class="col-12">
-            <table class="table">
-                <thead>
-                    <tr>   
-                        <th scope="col">Nome</th>
-                        <th scope="col">Descrizione</th>
-                        <th scope="col">Voto</th>
-                        <th scope="col">Distanza dal centro</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($hotels as $hotel){ ?>  
-                        <tr>
-                            <td><?php echo $hotel["name"] ?></td>
-                            <td><?php echo $hotel["description"] ?></td>
-                            <td><?php echo $hotel["vote"]." &#9734;" ?></td>
-                            <td><?php echo $hotel["distance_to_center"]."km" ?></td>
-                        </tr>
-                     <?php }  ?>
-                </tbody>
-                </table>
+        <main>
+            <div class="container mt-3">
+                <div class="row">
+                    <div class="col-12">
+                        <form action="./index.php" method="GET">
+                            <div class="row">
+                                <div class="col-4">
+                                    <select class="form-select" name="parking" id="parking">
+                                        <option selected>Seleziona filtro parcheggio</option>
+                                        <option value="1">Con parcheggio</option>
+                                        <option value="0">Senza parcheggio</option>
+                                    </select>
+                                </div>
+                                <d class="col-4">
+                                <button type="submit" class="btn btn-primary">Invia</button>
+                                </d>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
+            <div class="container my-5">
+                <div class="row">
+                    <div class="col-12">
+                    <table class="table">
+                        <thead>
+                            <tr>   
+                                <th scope="col">Nome</th>
+                                <th scope="col">Descrizione</th>
+                                <th scope="col">Voto</th>
+                                <th scope="col">Distanza dal centro</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($filter_hotel as $hotel){ ?>  
+                                <tr>
+                                    <td><?php echo $hotel["name"] ?></td>
+                                    <td><?php echo $hotel["description"] ?></td>
+                                    <td><?php echo $hotel["vote"]." &#9734;" ?></td>
+                                    <td><?php echo $hotel["distance_to_center"]."km" ?></td>
+                                </tr>
+                             <?php }  ?>
+                        </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>    
+        </main>
     <?php include __DIR__."/partials/templates/footer.php" ?>
-
-
-
-
 </body>
 </html>
